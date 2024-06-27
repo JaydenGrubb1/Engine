3526835f8d43ccc110874a852a762371f9fc031f
Jayden Grubb <jaydengrubb1@gmail.com>
2024-06-27 11:55:40 +1000

    Setup project file structure and build system

--- example/main.cpp
+++ example/main.cpp
@@ -1,12 +1,17 @@
 #include <cstdio>
 #include <cstdlib>
 
+#include <test.h>
+
 int main(int argc, char **argv) {
 	std::printf("Hello, World!\n");
 
 	for (int i = 0; i < argc; i++) {
 		std::printf("argv[%d] = %s\n", i, argv[i]);
 	}
+
+	auto ret = test(1, 2);
+	std::printf("test(1, 2) = %d\n", ret);
 
 	return EXIT_SUCCESS;
 }
